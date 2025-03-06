2bea55a260a28b270c6580614c8584033339fe79
unknown <vg2503@gmail.com>
2025-03-07 00:43:50 +0530

    added builder class and object creation

--- src/main/java/tictactoe/Client.java
+++ src/main/java/tictactoe/Client.java
@@ -1,20 +1,24 @@
 package tictactoe;
 
-import tictactoe.model.Game;
+import tictactoe.model.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
-        Game.GameBuilder gameBuilderObj = new Game.GameBuilder();
+        System.out.println("Welcome to TicTacToe Game");
+        Game.GameBuilder gameBuilderObj = Game.getGameBuilder();
+        Player player = new HumanPlayer(1, "John", new Symbol('#'));
+        Player player2 = new HumanPlayer(2, "Steve", new Symbol('0'));
+        Player player3 = new BotPlayer(3, "Bot James", new Symbol('^'), BotLevel.EASY);
+        List<Player> players = List.of(player, player2, player3);
         Game gameObj = gameBuilderObj
-                .setBoard(null)
-                .setMoves(new ArrayList<>())
-                .setPlayers(new ArrayList<>())
-                .setNextPlayerIndex(0)
-                .setWinner(null)
+                .setBoardSize(4)
+                .setPlayers(players)
                 .setWinningStrategy(new ArrayList<>())
                 .build();
+        System.out.println("Game Ends");
 
     }
 }
--- src/main/java/tictactoe/model/Game.java
+++ src/main/java/tictactoe/model/Game.java
@@ -12,59 +12,78 @@ public class Game {
     private int nextPlayerIndex;
     private GameState gameState;
     private List<WinningStrategy> winningStrategies;
+
+    public static GameBuilder getGameBuilder() {
+        return new GameBuilder();
+    }
+
     public Game(GameBuilder builder) {
-        this.board = builder.board;
+        this.board = new Board(builder.boardSize);
         this.players = builder.players;
-        this.moves = builder.moves;
-        this.nextPlayerIndex = builder.nextPlayerIndex;
-        this.winner = builder.winner;
-        this.gameState = builder.gameState;
+        this.moves = new ArrayList<>();
+        this.nextPlayerIndex = 0;
+        this.winner = null;
+        this.gameState = GameState.IN_PROGRESS;
         this.winningStrategies = builder.winningStrategy;
     }
     public static class GameBuilder{
-        private Board board;
+        private int boardSize;
         private List<Player> players;
-        private Player winner;
-        private List<Move> moves;
-        private int nextPlayerIndex;
         private List<WinningStrategy> winningStrategy;
-        private GameState gameState;
-        public GameBuilder setBoard(Board board){
-            this.board = board;
+
+        private void ValidateBotPlayer(){
+            int count = 0;
+            for (Player player : players) {
+                if(player.getPlayerType() == PlayerType.BOT){
+                    count++;
+                    if(count > 1){
+                        throw new RuntimeException("Game cannot have more than one BOT player");
+                    }
+                }
+            }
+        }
+
+        private void validateNoOfPlayers(){
+            if(players.size() >= boardSize){
+                throw new RuntimeException("Players cannot have more than size of board");
+            }
+        }
+
+        private void ValidateSymbols(){
+            HashSet<Character> symbolsSet = new HashSet<>();
+
+            for(Player player : players){
+                if(symbolsSet.contains(player.getSymbol().getPlayerSymbol())){
+                    throw new RuntimeException("Symbols cannot have more than one symbol");
+                }
+                symbolsSet.add(player.getSymbol().getPlayerSymbol());
+            }
+        }
+
+        private void validateGameObject(){
+            validateNoOfPlayers();
+            ValidateSymbols();
+            ValidateBotPlayer();
+        }
+
+        public GameBuilder setBoardSize(int boardSize){
+            this.boardSize = boardSize;
             return this;
         }
 
         public GameBuilder setPlayers(List<Player> players){
-            this.players = players;
-            return this;
-        }
-        public GameBuilder setWinner(Player winner){
-            this.winner = winner;
-            return this;
-        }
-
-        public GameBuilder setMoves(List<Move> moves){
-            this.moves = moves;
-            return this;
-        }
-
-        public GameBuilder setNextPlayerIndex(int nextPlayerIndex){
-            this.nextPlayerIndex = nextPlayerIndex;
+            this.players = new ArrayList<>(players);
             return this;
         }
 
         public GameBuilder setWinningStrategy(List<WinningStrategy> winningStrategy){
-            this.winningStrategy = winningStrategy;
-            return this;
-        }
-
-        public GameBuilder setGameState(GameState gameState){
-            this.gameState = gameState;
+            this.winningStrategy = new ArrayList<>(winningStrategy);
             return this;
         }
 
         public Game build(){
             //perform the validation here
+            validateGameObject();
             return new Game(this);
         }
 
